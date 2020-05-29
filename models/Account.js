875be94228bae13382f8b33c03fdbd30ee4382dd
gngfang <63154900+gngfang@users.noreply.github.com>
2020-05-28 23:34:59 -0400

    added Account schema and Transaction schema

---
+++ models/Account.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+    accountNumber: { type: String, required: true, unique: true },
+    accountType: { type: String, require: true },
+    institution: { type: String },
+    /* Will add transaction later */
+});
+
+
+const Account = mongoose.model('Account', accountSchema);
+module.exports = Account;
