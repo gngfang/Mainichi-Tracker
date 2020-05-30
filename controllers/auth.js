1504d8c0c808e29b86c8965592165ecbeb9e5b5d
gngfang <63154900+gngfang@users.noreply.github.com>
2020-05-30 17:45:25 -0400

    basic files and content for auth

---
+++ controllers/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const db = require('../models');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
