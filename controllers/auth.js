6770132c3541cd86855b3178d93671e4634857f7
gngfang <63154900+gngfang@users.noreply.github.com>
2020-05-30 23:39:03 -0400

    added register post route

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,25 +8,26 @@ router.get('/register', function (req, res) {
     res.render('auth/register')
 });
 
+router.post('/register', async function (req, res) {
+    try {
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if (foundUser) {
+            return res.send({ messsage: "Email already registered" })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password + hash
+        const newUser = db.User.create(req.body);
+        res.redirect('/login');
+
+    } catch (error) {
+
+        res.send({ messsage: "Internal Server Error" })
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
