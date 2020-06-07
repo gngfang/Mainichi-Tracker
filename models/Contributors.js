07d3745bdf65a45bebeadc53a18fc96995064839
xenia scullark <xeniascullark@xenias-MacBook-Pro.local>
2020-06-07 02:27:45 -0400

    added contributorsSchmea

--- models/Contributors.js
+++ models/Contributors.js
@@ -1,3 +1,13 @@
 
 const mongoose = require('mongoose');
 
+const contributorsSchema = new mongoose.Schema({
+    name: { type: String, },
+    about:{ type: String,},
+    
+    }, { timestamps: true });
+    
+    const Contributors = mongoose.model('Contributors', contributorsSchema);
+    module.exports = Contributors;
+    
+    
