db71f87adfbb90e8987c7bfadf1f050b40f8feed
xenia scullark <xeniascullark@xenias-MacBook-Pro.local>
2020-06-07 22:49:57 -0400

    added contributors html

--- controllers/contributors.js
+++ controllers/contributors.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+
+// about route
 router.get('/', function (req, res) {
     db.Contributors.find({}, function (error, findContributors) {
     if (error) {
@@ -14,9 +16,21 @@ router.get('/', function (req, res) {
     });
     });
 
-    
+    //create route
+        router.post('/', function (req, res) {
+            const contributors = {
+                name: req.body.name,
+                about: req.body.about,
+ }
 
-
+ db.Contributors.create(contributors, function (error, createdContributors) {
+    if (error) {
+        console.log(error);
+    } else {
+        res.redirect('/contributors, index');
+    }
+});
+});
 
 
 
